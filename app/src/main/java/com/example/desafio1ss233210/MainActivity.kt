c10cb138bab44775e487369cd71223793a4d0e14
jess300605 <sibrian3006@gmail.com>
2026-08-12 19:25:59 -0600

    feat: agregando strings, colores, dimensiones y navegacion del MainActivity"

--- app/src/main/java/com/example/desafio1ss233210/MainActivity.kt
+++ app/src/main/java/com/example/desafio1ss233210/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.desafio1ss233210
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnEjercicio1 = findViewById<Button>(R.id.btnEjercicio1)
+        val btnEjercicio2 = findViewById<Button>(R.id.btnEjercicio2)
+        val btnEjercicio3 = findViewById<Button>(R.id.btnEjercicio3)
+
+        btnEjercicio1.setOnClickListener {
+            val intent = Intent(this, PromedioActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnEjercicio2.setOnClickListener {
+            val intent = Intent(this, SalarioActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnEjercicio3.setOnClickListener {
+            val intent = Intent(this, CalculadoraActivity::class.java)
+            startActivity(intent)
         }
     }
 }
